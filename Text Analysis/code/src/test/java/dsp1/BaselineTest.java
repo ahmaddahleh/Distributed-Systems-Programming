099b531180eb70dc2310d0589773bf6a9ccef37e
Ahmad Dahleh <dahlehahmadbaha@gmail.com>
2026-07-21 20:36:06 +0300

    chore: establish clean and testable project baseline

---
+++ Text Analysis/code/src/test/java/dsp1/BaselineTest.java
@@ -0,0 +1,13 @@
+package dsp1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BaselineTest {
+
+    @Test
+    void testInfrastructureIsAvailable() {
+        assertTrue(true);
+    }
+}
